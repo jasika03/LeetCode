dfccc32432c6810cad2990a6336f56bc13bcad7a
Jasika <jasika1452@gmail.com>
2026-05-25 19:06:20 +0530

    Add files via upload

---
+++ Database/Confirmation_Rate.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+SELECT user_id, 
+ROUND(
+    AVG(
+        IF(action='confirmed',1.0,0.0))
+,2) 
+AS confirmation_rate
+FROM Signups
+LEFT JOIN Confirmations
+USING (user_id)
+GROUP BY user_id
