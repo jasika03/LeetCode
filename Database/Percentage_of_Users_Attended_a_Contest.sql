85d3c1b7f9b58c3124ba4fb88dd48db715475c71
Jasika <jasika1452@gmail.com>
2026-05-29 22:00:32 +0530

    Add files via upload

---
+++ Database/Percentage_of_Users_Attended_a_Contest.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+SELECT r.contest_id,
+ROUND((COUNT(r.user_id)*100/(SELECT COUNT(user_id) FROM Users)),2) AS percentage
+FROM Register r
+GROUP BY r.contest_id
+ORDER BY percentage DESC, r.contest_id;
